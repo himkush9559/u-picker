55869cbd2dc5795c5834ebd33d52ae53fae0b6da
Himanshu Kushwaha <136429466+himkush9559@users.noreply.github.com>
2024-01-17 00:03:21 +0530

    .

---
+++ index.js
@@ -0,0 +1,22 @@
+let userNames = [
+  "Ansh",
+  "Deepak",
+  "Deepanshu",
+  "Aman",
+  "Shubham",
+  "Vaibhav",
+  "saloni",
+  "salini",
+  "Amit",
+  "suraj",
+  "priya",
+  "Arjun",
+  "Nikhil",
+  "Amit",
+  "sonu",
+];
+
+let formPicker = document.querySelector("#pick-user-form");
+let usernameInput = document.querySelector("#user-input");
+
+const opts = { multiple: true };
